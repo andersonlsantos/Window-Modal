ae2bd9bffe7f433059ec7814a5cd1cf9811bbc33
Anderson Ladislau dos Santos <73796983+andersonlsantos@users.noreply.github.com>
2022-07-08 15:39:43 -0300

    hidden

--- script.js
+++ script.js
@@ -2,10 +2,24 @@
 
 const sellAll = idClass => document.querySelectorAll(`${idClass}`)
 
+const sell = idClass => document.querySelector(`${idClass}`)
+
+function hidden(){
+    for(let cont = 0; cont < sellAll('.hidden').length; cont++){
+        sellAll('.hidden')[cont].style.display = 'block'
+    }
+}
+
 const controlClick = () => {
-    
+    hidden()
+}
+
+const controlClick2 = () => {
+    hidden()
 }
 
 for(let cont = 0; cont < sellAll('.show-modal').length; cont++){
     sellAll('.show-modal')[cont].addEventListener('click', controlClick)
 }
+
+sell('close-modal').addEventListener('click', controlClick2)
