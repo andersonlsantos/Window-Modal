af46952f5dd97a87bda272fd2fe336f0544123e7
Anderson Ladislau dos Santos <73796983+andersonlsantos@users.noreply.github.com>
2022-07-08 15:49:20 -0300

    close-modal

--- script.js
+++ script.js
@@ -4,22 +4,22 @@ const sellAll = idClass => document.querySelectorAll(`${idClass}`)
 
 const sell = idClass => document.querySelector(`${idClass}`)
 
-function hidden(){
+function hidden(add){
     for(let cont = 0; cont < sellAll('.hidden').length; cont++){
-        sellAll('.hidden')[cont].style.display = 'block'
+        sellAll('.hidden')[cont].style.display = add
     }
 }
 
 const controlClick = () => {
-    hidden()
+    hidden('block')
 }
 
 const controlClick2 = () => {
-    hidden()
+    hidden('none')
 }
 
 for(let cont = 0; cont < sellAll('.show-modal').length; cont++){
     sellAll('.show-modal')[cont].addEventListener('click', controlClick)
 }
 
-sell('close-modal').addEventListener('click', controlClick2)
+sell('.close-modal').addEventListener('click', controlClick2)
