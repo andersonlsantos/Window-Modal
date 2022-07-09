2f0c44a815c46bd623f5c7f7fc4e8088bcfe6977
Anderson Ladislau dos Santos <73796983+andersonlsantos@users.noreply.github.com>
2022-07-09 16:57:03 -0300

    Last commit

--- script.js
+++ script.js
@@ -16,9 +16,17 @@ const controlClick2 = () => hidden('none')
 
 const controlClick3 = () => hidden('none')
 
-document.onkeydown = e => {
+// document.onkeydown = e => {
+//     if(e.key === 'Escape') hidden('none')
+// } 
+
+// keyup - lift finger of
+// keypress - keep finger
+// keydown - soon press down the key
+
+document.addEventListener('keydown', e => {
     if(e.key === 'Escape') hidden('none')
-} 
+})
 
 for(let cont = 0; cont < sellAll('.show-modal').length; cont++){
     sellAll('.show-modal')[cont].addEventListener('click', controlClick)
